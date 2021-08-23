4d9f6059b9317db49f81036a3c0c1326ed154b0d
AlexMc124 <alex.mcintosh124@gmail.com>
2021-08-23 16:15:16 +0100

    finished the madlib game

--- madlib.php
+++ madlib.php
@@ -16,18 +16,25 @@
     </p>
     <hr>
     This will be used to collect Words from the user and make a story in the style of a Madlib. <br>
-    This will be done with the $_GET Method and the name of the Form variable.
+    This will be done with the $_GET Method and the name of the Form variable. <br> <br>
     
-    <form action="userinput.php" method="get">
-        Name : <input type="text" name="name"></input>
+    <form action="madlib.php" method="get">
+        Colour : <input type="text" name="color"></input>
         <br>
-        Age : <input type="text" name="age"></input>
+        Plural Noun : <input type="text" name="noun"></input>
+        <br>
+        Celebrity : <input type="text" name="celeb"></input>
         <br>
         <button type="submit">Submit</button>
     </form>
     <br>
-    Your name is <?php echo $_GET["name"]?>
-    <br>
-    Your age is <?php echo $_GET["age"]?>
+    <?php 
+        $color = $_GET["color"];
+        $noun = $_GET["noun"];
+        $celeb = $_GET["celeb"];
+        echo "Roses are $color <br>";
+        echo "$noun are blue <br>";
+        echo "I love $celeb <br>";
+    ?>
 </body>
 </html>
