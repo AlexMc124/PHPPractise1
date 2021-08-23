7b9deb7bf05343dacf58ad612c2eb9b33a0dbccb
AlexMc124 <alex.mcintosh124@gmail.com>
2021-08-23 16:24:23 +0100

    Changed the Post method of UserInput to be POST

--- userinput.php
+++ userinput.php
@@ -16,18 +16,17 @@
     </p>
     <hr>
     This will be used to collect the users Name & Age through a form. <br>
-    This will be done with the $_GET Method and the name of the Form variable.
-    
-    <form action="userinput.php" method="get">
-        Name : <input type="text" name="name"></input>
-        <br>
-        Age : <input type="text" name="age"></input>
-        <br>
+    This will be done with the $_GET Method and the name of the Form variable.<br>
+    This has also been used to learn URL Parameter.<br> The Method of the form has been changed to POST to prevent URL Tampering. <br><br>
+    <hr>
+    <form action="userinput.php" method="post">
+        Name : <input type="text" name="name"></input><br>
+        Age : <input type="text" name="age"></input><br>
         <button type="submit">Submit</button>
     </form>
     <br>
-    Your name is <?php echo $_GET["name"]?>
+    Your name is <?php echo $_POST["name"]?>
     <br>
-    Your age is <?php echo $_GET["age"]?>
+    Your age is <?php echo $_POST["age"]?>
 </body>
 </html>
